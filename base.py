9945d98bed24081822d50f9162f7ece79a6d2fdf
lswhim <1519027662@qq.com>
2024-10-17 09:59:07 +0800

    update

--- base.py
+++ base.py
@@ -13,11 +13,10 @@ class AbstractModel(nn.Module):
         total_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
         return f'Total number of trainable parameters: {total_params}'
 
-    def calculate_loss(self, batch):
-        raise NotImplementedError('calculate_loss method must be implemented.')
+    def get_representation(self, batch):
+        raise NotImplementedError('get representation method must be implemented.')
+    def get_embeddings(self, items):
+        raise NotImplementedError('get item_embeddings must be implemented.')
 
     def predict(self, batch, n_return_sequences=1):
         raise NotImplementedError('predict method must be implemented.')
-
-    def get_embeddings(self, items):
-        raise NotImplementedError('get item_embeddings must be implemented.')
