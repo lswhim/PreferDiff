5797bd70c988a88039f3843dc9cb73b69e0f4fd6
lswhim <1519027662@qq.com>
2024-10-17 09:40:14 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,14 +8,15 @@ from typing import Union, Optional
 
 from base import AbstractModel
 
-
+# Initialize random seeds to ensure reproducibility across different libraries (random, numpy, torch).
+# It also configures CUDA and cuDNN for reproducible results if required.
 def init_seed(seed: int, reproducibility: bool):
     """
     Initialize random seeds for reproducibility across random functions in numpy, torch, cuda, and cudnn.
 
     Args:
         seed (int): Random seed value.
-        reproducibility (bool): Whether to enforce reproducibility.
+        reproducibility (bool): Whether to enforce reproducibility by making operations deterministic.
     """
     import random
     import numpy as np
@@ -30,7 +31,7 @@ def init_seed(seed: int, reproducibility: bool):
     torch.backends.cudnn.benchmark = not reproducibility
     torch.backends.cudnn.deterministic = reproducibility
 
-
+# Get the current local time in the format: "Month-Day-Year_Hour-Minute-Second".
 def get_local_time() -> str:
     """
     Get the current local time in a specific format.
@@ -40,7 +41,8 @@ def get_local_time() -> str:
     """
     return datetime.datetime.now().strftime("%b-%d-%Y_%H-%M-%S")
 
-
+# Retrieve the command line arguments passed during script execution and return them as a single string,
+# with '/' replaced by '|'.
 def get_command_line_args_str() -> str:
     """
     Get the command line arguments as a single string, with '/' replaced by '|'.
@@ -50,7 +52,8 @@ def get_command_line_args_str() -> str:
     """
     return '_'.join(sys.argv).replace('/', '|')
 
-
+# Get the model class or instance based on the provided model name. If the model name is provided as a string,
+# it imports the model dynamically.
 def get_model(model_name: Union[str, AbstractModel]) -> AbstractModel:
     """
     Retrieve the model class based on the provided model name.
@@ -74,29 +77,8 @@ def get_model(model_name: Union[str, AbstractModel]) -> AbstractModel:
 
     return model_class
 
-def get_mapper(model_name: str):
-    """
-    Retrieves the mapper for a given model name.
-
-    Args:
-        model_name (str): The model name.
-
-    Returns:
-        AbstractMapper: The tokenizer for the given model name.
-
-    Raises:
-        ValueError: If the tokenizer is not found.
-    """
-    try:
-        mapper_class = getattr(
-            importlib.import_module(f'models.{model_name}._mapper'),
-            f'{model_name}Mapper'
-        )
-    except:
-        raise ValueError(f'Mapper for model "{model_name}" not found.')
-    return mapper_class
-
-
+# Parse command line arguments in the format '--key=value' and return them as a dictionary.
+# It raises an error if the format is incorrect.
 def parse_command_line_args(unparsed: list[str]) -> dict:
     """
     Parse command line arguments into a dictionary.
@@ -124,7 +106,8 @@ def parse_command_line_args(unparsed: list[str]) -> dict:
 
     return args
 
-
+# Initialize the device for training, selecting either a GPU if available or CPU.
+# It also checks if Distributed Data Parallel (DDP) is enabled.
 def init_device() -> tuple:
     """
     Set the visible devices for training, supporting multiple GPUs.
@@ -139,7 +122,8 @@ def init_device() -> tuple:
 
     return device, use_ddp
 
-
+# Get the configuration dictionary from multiple sources: default, model-specific, user-specified config files,
+# and an optional config dictionary. The final merged configuration is returned.
 def get_config(
         model_name: Union[str, AbstractModel],
         config_file: Union[str, list[str], None],
@@ -150,7 +134,6 @@ def get_config(
 
     Args:
         model_name (Union[str, AbstractModel]): The name or instance of the model.
-        dataset_name (Union[str, AbstractDataset]): The name or instance of the dataset.
         config_file (Union[str, list[str], None]): Additional configuration file(s).
         config_dict (Optional[dict]): Dictionary of additional configuration options.
 
@@ -163,7 +146,6 @@ def get_config(
     final_config = {}
     current_path = os.path.dirname(os.path.realpath(__file__))
     config_file_list = [os.path.join(current_path, 'default.yaml')]
-
 
     if isinstance(model_name, str):
         config_file_list.append(os.path.join(current_path, f'models/{model_name}/config.yaml'))
@@ -188,6 +170,8 @@ def get_config(
     final_config['run_local_time'] = get_local_time()
     return convert_config_dict(final_config)
 
+# Calculate the total number of training steps using either a fixed number of steps or based on the number of
+# epochs and the length of the training dataloader.
 def get_total_steps(config, train_dataloader):
     """
     Calculate the total number of steps for training based on the given configuration and dataloader.
@@ -198,13 +182,13 @@ def get_total_steps(config, train_dataloader):
 
     Returns:
         int: The total number of steps for training.
-
     """
     if config['steps'] is not None:
         return config['steps']
     else:
         return len(train_dataloader) * config['epochs']
 
+# Convert configuration values from string format to their respective data types, such as boolean, integer, or list.
 def convert_config_dict(config: dict) -> dict:
     """
     Convert configuration values in a dictionary to their appropriate types.
@@ -227,7 +211,19 @@ def convert_config_dict(config: dict) -> dict:
 
     return config
 
+# Generate a unique filename based on the configuration, command line arguments, and a timestamp.
+# The file name includes an MD5 hash of the configuration for uniqueness.
 def get_file_name(config: dict, suffix: str = ''):
+    """
+    Generate a unique log or output filename using the configuration, command line args, and a timestamp.
+
+    Args:
+        config (dict): The configuration dictionary.
+        suffix (str, optional): A suffix to append to the filename.
+
+    Returns:
+        str: A unique filename.
+    """
     import hashlib
     config_str = "".join([str(value) for key, value in config.items() if key != 'accelerator'])
     md5 = hashlib.md5(config_str.encode(encoding="utf-8")).hexdigest()[:6]
@@ -236,4 +232,3 @@ def get_file_name(config: dict, suffix: str = ''):
         config["run_id"], command_line_args, config['run_local_time'], md5, suffix
     )
     return logfilename
-
